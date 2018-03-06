046e3558d3bce8b0e25b672b82d4ca250fdaa918
kaltkabel <34767233+kaltkabel@users.noreply.github.com>
2018-03-06 16:35:53 +0100

    js update & support for new devices

--- v2/javascript/Include.js
+++ v2/javascript/Include.js
@@ -2,13 +2,16 @@ function Include() {
 	var opt1 = $("#mod option:selected").attr('class');
 	var opt2 = $("#mod option:selected").val();
 	var file = opt1+"_"+opt2;
-	$('#submit').prop('disabled', true).delay(700).queue(
+	$("#include").css("opacity", "0");
+	$('#submit').prop('disabled', true).delay(1000).queue(
 		function(next) {
 		$(this).prop('disabled', false);
 		next(); 
+		}); 
+	$("#include").delay(200).queue(function( nxt ) {
+		$(this).load("includes/"+file);
+		nxt();
 		});
-	$("#include").load("includes/"+file); 
-	$("#include").css("opacity", "0");
-	$("#include").delay('500');
+	$("#include").delay(600);
 	$("#include").animate({opacity: '1'}, "fast");
 }
